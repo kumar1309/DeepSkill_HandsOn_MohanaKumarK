9223a00352f4f7a7980006ea4006fb55704b4ae3
kumar1309 <160880538+kumar1309@users.noreply.github.com>
2025-06-29 17:47:33 +0530

    Week 2 Additional HandsOn
    Week 2 Additional HandsOn

---
+++ Week2_PLSQL_JUnit_Mockito_SJ4J/Junit,MockitoAndSL4J/02_JUnit_Advanced_Exercise/01_Parameterized_Tests/code/demo/src/test/java/com/example/EvenCheckerTest.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class EvenCheckerTest {
+
+    private final EvenChecker evenChecker = new EvenChecker();
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 8, 10})
+    void testIsEvenWithEvenNumbers(int number) {
+        assertTrue(evenChecker.isEven(number), number + " should be even");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, 7, 9})
+    void testIsEvenWithOddNumbers(int number) {
+        assertFalse(evenChecker.isEven(number), number + " should not be even");
+    }
+}
